06f3f2b0e2ad555b292a352628f1b3a9e4e0a844
meat.carver <meat.carver@604d0904-2932-11de-a0a5-a9ea28672ce9>
2009-05-09 22:09:21 +0000

    * added command handling to regular chat events and removed events.OnChat * changed config.owner to be uuid * added error handling for syntax errors when reloading * added commands.come

--- trunk/commands.py
+++ trunk/commands.py
@@ -6,7 +6,7 @@ import bot
 def default(client, msg):
 	"""default handler for instant messages"""
 
-	print "[%s/im] %s: %s" % (client.Self.Name, msg.FromAgentName, msg.Message)
+	print "[%s] %s: %s" % (client.Self.Name, msg.FromAgentName, msg.Message)
 
 @bot.Command(public=True)
 def tp(client, msg):
@@ -19,3 +19,12 @@ def quit(client, msg):
 	"""logout"""
 	
 	client.Network.Logout()
+	
+@bot.Command()
+def come(client, msg):
+	"""walk to the sender"""
+	
+	pos = (bot.Vector3(client.Self.GlobalPosition) - 
+		client.Self.SimPosition + msg.Position)
+	client.Self.AutoPilotCancel()
+	client.Self.AutoPilot(pos.X, pos.Y, client.Self.SimPosition.Z)
